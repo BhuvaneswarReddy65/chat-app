ce416d7b8d472bcfb1268d6b91aa8aa2bfeaea91
Bhuvaneswar <bhuvaneswar5251@gmail.com>
2023-12-27 12:19:51 +0530

    changing the branch

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./myStyles.css";
+import Sidebar from "./Sidebar";
+import WorkArea from "./WorkArea";
 const MainContainer = () => {
-  return <div className="main-container">MainContainer</div>;
+  return (
+    <div className="main-container">
+      <Sidebar />
+      <WorkArea />
+    </div>
+  );
 };
 
 export default MainContainer;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./myStyles.css";
+const Sidebar = () => {
+  return <div className="sidebar-container"></div>;
+};
+
+export default Sidebar;
