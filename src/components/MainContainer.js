ed86189a4f5de67cd40bf8bf15023ff241b9cde0
Bhuvaneswar <bhuvaneswar5251@gmail.com>
2023-12-27 11:49:20 +0530

    initial-setup

---
+++ src/components/MainContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./myStyles.css";
+const MainContainer = () => {
+  return <div className="main-container">MainContainer</div>;
+};
+
+export default MainContainer;
