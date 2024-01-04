2561fcbae73b1f2be58f1e3942b952b4ca3b3458
Bhuvaneswar <bhuvaneswar5251@gmail.com>
2024-01-04 13:43:55 +0530

    all changes are doe

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,28 @@
 import React from "react";
 import logo from "./images/live-chat.png";
 import { useSelector } from "react-redux";
-
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 const Welcome = () => {
   const lightTheme = useSelector((state) => state.themekey);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  console.log(userData);
+  const nav = useNavigate();
+  if (!userData) {
+    console.log("User not Authenticated");
+    nav("/");
+  }
   return (
     <div
       className={lightTheme ? "welcome-container" : "welcome-container dark"}
     >
-      <img src={logo} alt="logo" className="welcome-logo" />
+      <motion.img
+        src={logo}
+        alt="logo"
+        className="welcome-logo"
+        whileTap={{ scale: 1.05, rotate: 360 }}
+      />
+      <b>Hi , {userData.data.name} 👋</b>
       <p>View and text directly to people present in the chat Rooms</p>
     </div>
   );
